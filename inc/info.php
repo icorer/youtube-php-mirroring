e8fbf877ac433202ab39f98a4169e96a056773b1
CorerMan <corerman@users.noreply.github.com>
2016-09-14 12:16:05 +0800

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -1,8 +1,8 @@
 ﻿<?php
 //
-$ym='http://abc.net/';//你的站点域名。不可为空，否则解析器无法工作
+$ym='https://y2b.icorer.com/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
-$home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
+$home_v='UCb3TZ4SD_Ys3j4z0-8o6auA';//首页推荐视频用户身份标示
 $youtube_api='你的api';//youtube视频API
 $gurl_api='你的api';//谷歌缩址api，用于防止dns污染
 $title='Youtube镜像';//您的网站名字
